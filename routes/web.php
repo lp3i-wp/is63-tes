c9f8fff06de4fd660188328d4815820cbe82cae5
is61b <is61blp3i@gmail.com>
2026-04-16 09:59:49 +0700

    controller mahasiswa part 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,7 @@ Route::get('/', function () {
 Route::get('/contoh1', function () {
     return view('lp3i');
 });
+
+//Modul Mahasiswa
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('/mahasiswa/tambah', [MahasiswaController::class, 'create']);

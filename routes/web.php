86a89a8ac0d0a6b2220e540935609b31baddf443
is61b <is61blp3i@gmail.com>
2026-04-16 12:05:04 +0700

    modul mahasiswa 1 (index)

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/contoh1', function () {
     return view('lp3i');
 });
 
+// Route::get('/master', function () {
+//     return view('layout.master');
+// });
+
 //Modul Mahasiswa
 Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
 Route::get('/mahasiswa/tambah', [MahasiswaController::class, 'create']);
